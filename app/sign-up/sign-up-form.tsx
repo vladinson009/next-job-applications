02211cd7ba23662c7a12e299629613555c8db1db
Vladimir Gulev <vladinson009@icloud.com>
2026-02-02 00:05:52 +0100

    Add: Shadcn components

---
+++ app/sign-up/sign-up-form.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { signUpSchema } from '@/validations/userValidation';
+import { useForm } from 'react-hook-form';
+import z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+type SignUpSchema = z.infer<typeof signUpSchema>;
+type DataOutput = z.output<typeof signUpSchema>;
+
+export default function SignUpForm() {
+  const form = useForm<SignUpSchema>({
+    resolver: zodResolver(signUpSchema),
+  });
+
+  function onSubmit(data: DataOutput) {
+    console.log(data);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel></FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
