3e070abad946c772b89e1330873a649fa351a3a1
Vladimir Gulev <vladinson009@icloud.com>
2026-02-01 22:49:45 +0100

    Add: Navbar component

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,32 @@
+import { BriefcaseBusiness } from 'lucide-react';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import Container from '../container';
+
+export default function Navbar() {
+  return (
+    <Container className="pt-1 md:pt-2">
+      <header className="flex items-center justify-between">
+        <Link className="hover:text-color1 transition-colors" href="/">
+          <BriefcaseBusiness size={50} />
+        </Link>
+        <ul className="flex gap-2">
+          <li>
+            <Button variant="default" asChild>
+              <Link href="sign-in">Sign In</Link>
+            </Button>
+          </li>
+          <li>
+            <Button
+              className="bg-color1 hover:bg-color2 hover:text-color3"
+              variant="secondary"
+              asChild
+            >
+              <Link href="sign-up">Sign Up</Link>
+            </Button>
+          </li>
+        </ul>
+      </header>
+    </Container>
+  );
+}
