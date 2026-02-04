c64a3055b0cca41ae79e1384cdb43e5c71042229
Vladimir Gulev <vladinson009@icloud.com>
2026-02-04 22:30:18 +0100

    Add: Sign in action

---
+++ app/sign-in/actions/sign-in-action.ts
@@ -0,0 +1,19 @@
+'use server';
+import type { SignInOutputSchema } from '@/types/User';
+
+import { signIn } from '@/auth';
+import { isRedirectError } from '@/lib/redirectError';
+
+export async function handleSignIn(data: SignInOutputSchema) {
+  try {
+    await signIn('credentials', {
+      ...data,
+      redirectTo: '/',
+    });
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return { message: 'Invalid credentials' };
+  }
+}
