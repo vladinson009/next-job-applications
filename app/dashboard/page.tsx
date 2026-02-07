0c8854e6053ac65d7477c9a7ec0174bdc035fca1
Vladimir Gulev <vladinson009@icloud.com>
2026-02-07 21:30:21 +0100

    Add: Dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,65 @@
+import Container from '@/components/container';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import Link from 'next/link';
+import { fetchBoards } from './actions/fetchBoards';
+import { MoreVerticalIcon } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import DeleteBoardButton from './delete-board-button';
+import EditBoardButton from './edit-board-button';
+
+export default async function DashboardPage() {
+  const boards = await fetchBoards();
+
+  return (
+    <section>
+      <Container className="my-5 xl:my-15 space-y-5">
+        <Button asChild>
+          <Link href="/dashboard/create-board">Create new board</Link>
+        </Button>
+        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+          {boards.map((board) => (
+            <Card key={board.id} className="">
+              <CardHeader>
+                <CardTitle>{board.name}</CardTitle>
+                <CardDescription>
+                  <p>Created at: {board.createdAt.toLocaleDateString()}</p>
+                  <p>Last update: {board.updatedAt.toLocaleDateString()}</p>
+                </CardDescription>
+                <CardAction>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <MoreVerticalIcon />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      {/* Content */}
+                      <DeleteBoardButton board={board} />
+                      <EditBoardButton board={board} />
+                      {/* Content */}
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </CardAction>
+              </CardHeader>
+              <CardContent>
+                <Button variant="link" asChild>
+                  <Link href={`/dashboard/${board.id}`}>Show board</Link>
+                </Button>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </Container>
+    </section>
+  );
+}
