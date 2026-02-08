aabea45245283da300e84925c4d2f9d64cb75504
Vladimir Gulev <vladinson009@icloud.com>
2026-02-08 01:21:08 +0100

    Add: Zod schema for job

---
+++ validations/jobValidation.ts
@@ -0,0 +1,21 @@
+import z from 'zod';
+
+export const jobSchema = z.object({
+  title: z
+    .string('Title must be a text')
+    .min(1, 'Title is required')
+    .max(100, 'Title length is too long. Limit 100 characters'),
+  companyName: z
+    .string('Company name must be a text')
+    .min(1, 'Company name is required')
+    .max(255, 'Company name length is too long. Limit 255 characters'),
+  location: z
+    .string('Location must be a text')
+    .min(1, 'Location is required')
+    .max(255, 'Location length is too long. Limit 255 characters'),
+  salary: z.coerce.number<number>('Salary must be a number').optional(),
+  remote: z
+    .enum(['true', 'false'], 'Remote must be a boolean')
+    .default('false')
+    .nonoptional(),
+});
