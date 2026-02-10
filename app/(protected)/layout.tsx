c4999359ac2d589ee15ca2352ca8067c82f378e2
Vladimir Gulev <vladinson009@icloud.com>
2026-02-10 21:02:46 +0100

    #

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,6 +1,8 @@
 import { requireUser } from '@/lib/auth-server';
 import { PropsWithChildren } from 'react';
 
+export const dynamic = 'force-dynamic';
+
 export default async function ProtectedLayout({ children }: PropsWithChildren) {
   await requireUser();
   return <>{children}</>;
